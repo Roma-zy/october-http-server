07b83722275771ebeecb780842bec779670e7f2c
Roman Yuzyuk <romanyuzyukfront@gmail.com>
2024-10-12 14:17:33 +0300

    Add query processing in a loop

--- src/main/java/ru/otus/october/http/server/Application.java
+++ src/main/java/ru/otus/october/http/server/Application.java
@@ -2,7 +2,6 @@ package ru.otus.october.http.server;
 
 public class Application {
     // Домашнее задание:
-    // 1. Добавить обработку запросов в цикле
     // 2. Добавить обработку запросов в тред пуле
 
     public static void main(String[] args) {
