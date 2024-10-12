a96a0362bbe22fd5e6e8ee0586141ef411f3c810
Roman Yuzyuk <romanyuzyukfront@gmail.com>
2024-10-12 14:30:03 +0300

    Add request processing to the thread pool

--- src/main/java/ru/otus/october/http/server/Application.java
+++ src/main/java/ru/otus/october/http/server/Application.java
@@ -1,10 +1,7 @@
 package ru.otus.october.http.server;
 
 public class Application {
-    // Домашнее задание:
-    // 2. Добавить обработку запросов в тред пуле
-
-    public static void main(String[] args) {
-        new HttpServer(8189).start();
+   public static void main(String[] args) {
+        new HttpServer(8189, 4).start();
     }
 }
--- src/main/java/ru/otus/october/http/server/HttpServer.java
+++ src/main/java/ru/otus/october/http/server/HttpServer.java
@@ -3,33 +3,44 @@ package ru.otus.october.http.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
     private int port;
     private Dispatcher dispatcher;
+    private ExecutorService threadPool;
 
-    public HttpServer(int port) {
+    public HttpServer(int port, int threadPoolSize) {
         this.port = port;
         this.dispatcher = new Dispatcher();
+        this.threadPool = Executors.newFixedThreadPool(threadPoolSize);
     }
 
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    byte[] buffer = new byte[8192];
-                    int n = socket.getInputStream().read(buffer);
-                    String rawRequest = new String(buffer, 0, n);
-                    HttpRequest request = new HttpRequest(rawRequest);
-                    request.info(true);
-                    dispatcher.execute(request, socket.getOutputStream());
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                }
+                Socket socket = serverSocket.accept();
+                threadPool.execute(() -> handleRequest(socket));
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
+    }
+
+    private void handleRequest(Socket socket) {
+        try {
+            byte[] buffer = new byte[8192];
+            int n = socket.getInputStream().read(buffer);
+            String rawRequest = new String(buffer, 0, n);
+            HttpRequest request = new HttpRequest(rawRequest);
+            request.info(true);
+            dispatcher.execute(request, socket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
