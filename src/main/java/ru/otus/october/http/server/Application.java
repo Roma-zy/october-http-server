e95bf210a9a3b1d5f07f9e121e6b5d0df6db73a6
Roman Yuzyuk <romanyuzyukfront@gmail.com>
2024-10-17 19:56:34 +0300

    remove comment

--- src/main/java/ru/otus/october/http/server/Application.java
+++ src/main/java/ru/otus/october/http/server/Application.java
@@ -1,9 +1,6 @@
 package ru.otus.october.http.server;
 
 public class Application {
-    // Домашнее задание:
-    // 1. Избавиться от sout'ов и подключить логгер
-    // 3. * Добавьте обработку 405 Method Not Allowed
    public static void main(String[] args) {
         new HttpServer(8189, 4).start();
     }
